f58369cd740b470a192121478166c9801c296b1c
Marcos Marques <97256504+MarcosMMarques@users.noreply.github.com>
2022-07-13 12:44:44 -0300

    Update Fibonacci.c

--- C algorithms/ICC/Fibonacci.c
+++ C algorithms/ICC/Fibonacci.c
@@ -5,22 +5,20 @@
 int main(){
   int n,i;
   scanf("%d",&n);
-  //Aqui lemos a quantidade de termos que o usuario quer que seja impresso na tela.
+  
+  /*Para que seja possivel somarmos a primeira casa com 0, criamos um vetor com o numero de termos que o usuario quer
+  *com uma casa a mais, para que seja possivel a realização da sequencia.*/
   int v[n+1];
   v[0]=0;
-  //Para que seja possivel somarmos a primeira casa com 0, criamos um vetor com o numero de termos que o usuario quer
-  //com uma casa a mais, para que seja possivel a realização da sequencia.
+  
   for(i=1;i<n+1 && n>0;i++){
-      //O laco de repeticao so sera executado se o numero digitado pelo usuario for maior que 0.
       v[i]=v[i-1]+v[i-2];
-      //Aqui e realizado a operacao das duas casas antecedentes a casa desejada.
       if(i==1){
         v[i]=1;
         //Como os primero termo é 1, atribuimos a segunda posicao do vetor 1, para que a proxima casa
         //seja realizado a conta corretamente, 0+1=1,1+1=2, 2+1=3, etc.
       }
       printf("%d ",v[i]);
-      //Aqui e exibido na tela os n termos da sequencia de Fibonacci.
   }
   return 0;
 }
