54607e699559786cec314b85b1cfc7255e1d30b9
Marcos Marques <97256504+MarcosMMarques@users.noreply.github.com>
2022-07-13 13:21:20 -0300

    Clean code method
    Clean comments

--- C algorithms/ICC/Recurssion.c
+++ C algorithms/ICC/Recurssion.c
@@ -1,9 +1,9 @@
 //This code is a study for recurssion method used for calculate a factorial of any number.
 
 #include <stdio.h>
-int factorial(int number){//The function factorial calculate the factorial of digited number by the user
+int factorial(int number){
     if (number == 0){
-        return 1;//When the factorial becomes zero, returns the final of factorial of any number, the number one
+        return 1;
     }else{
         return number * factorial(number-1);/*The variable a is a digited number by the user and when calling function,
          *if variable a is diferent of zero, it will return the variable a multiplied by the factorial function itself but number-1,
@@ -15,7 +15,7 @@ int factorial(int number){//The function factorial calculate the factorial of di
 int main(){
     int number;
     printf("Insert a number for calculate his factorial: ");
-    scanf("%d", &number);//Scanning a number and storing it in the number variable
-    printf("The factorial of the number %d is: %d\n", number, factorial(number));//Calling function factorial
+    scanf("%d", &number);
+    printf("The factorial of the number %d is: %d\n", number, factorial(number));
     return 0;
 }
