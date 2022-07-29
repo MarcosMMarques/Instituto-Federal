70e33cc0cf0b62c7ae4dab005a2760dab2700b9e
Marcos Marques <97256504+MarcosMMarques@users.noreply.github.com>
2022-07-29 15:33:05 -0300

    Update Split_Strings.py

--- Python/Split_Strings.py
+++ Python/Split_Strings.py
@@ -1,20 +1,20 @@
 if __name__ == '__main__':
-    N = int(raw_input())
-    vlist=[]
-    while N>0:
-        command=(raw_input())
-        if command.split()[0]=="insert":
-            vlist.insert(int(command.split()[1]),int(command.split()[2]))
-        elif command.split()[0]=="print":
-            print(vlist)
-        elif command.split()[0]=="remove":
-             vlist.remove(int(command.split()[1]))
-        elif command.split()[0]=="append":
-            vlist.append(int(command.split()[1]))
-        elif command.split()[0]=="sort":
-            vlist.sort()
-        elif command.split()[0]=="pop":
-            vlist.pop()
-        elif command.split()[0]=="reverse":
-            vlist.reverse()
-        N=N-1
+    number = int(raw_input())
+    values_list=[]
+    while number > 0:
+        command = (raw_input())
+        if command.split()[0] == "insert":
+            values_list.insert(int(command.split()[1]), int(command.split()[2]))
+        elif command.split()[0] == "print":
+            print(value_list)
+        elif command.split()[0] == "remove":
+             value_list.remove(int(command.split()[1]))
+        elif command.split()[0] == "append":
+            value_list.append(int(command.split()[1]))
+        elif command.split()[0] == "sort":
+            value_list.sort()
+        elif command.split()[0] == "pop":
+            value_list.pop()
+        elif command.split()[0] == "reverse":
+            value_list.reverse()
+        number= number - 1
