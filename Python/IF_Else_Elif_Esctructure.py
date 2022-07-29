fb827447a42abf5097d78f7d58bd4024d4a15206
Marcos Marques <97256504+MarcosMMarques@users.noreply.github.com>
2022-07-29 15:17:55 -0300

    Update IF_Else_Elif_Esctructure.py

--- Python/IF_Else_Elif_Esctructure.py
+++ Python/IF_Else_Elif_Esctructure.py
@@ -1,21 +1,17 @@
-#!/bin/python3
-
-import math
 import os
+import math
 import random
 import re
 import sys
 
 
-
 if __name__ == '__main__':
-    n = int(input().strip())
-    if n%2!=0:
+    number = int(input().strip())
+    if number%2 != 0:
+        print("Weird")
+    elif number>=2 and number<=5:
+        print("Not Weird")
+    elif number>=6 and number<=20:
         print("Weird")
     else:
-        if n>=2 and n<=5:
-            print("Not Weird")
-        elif n>=6 and n<=20:
-            print("Weird")
-        else:
-            print("Not Weird")
+        print("Not Weird")
