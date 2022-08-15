210d1c2588904d1727a5543550cdd06dbf1be376
Marcos Marques <97256504+MarcosMMarques@users.noreply.github.com>
2022-08-15 16:56:53 -0300

    Add files via upload

---
+++ C algorithms/High_School_Algorithms/Exercicio 2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int n;
+	printf("Escreva um numero ");
+	scanf("%d",&n);
+	printf("O numero informado foi: %d",n);
+	return 0;
+}
---
+++ C algorithms/High_School_Algorithms/Exercicio 3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int n1,n2,s;
+	printf("Escreva dos numeros ");
+	scanf("%d",&n1);
+		scanf("%d",&n2);
+		s=n1+n2;
+	printf("Soma: %d",s);
+	return 0;
+}
